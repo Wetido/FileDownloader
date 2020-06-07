4c9d2b8cca0dce24344cef196337c43b92b49f87
Jakub <wetido@gmail.com>
2020-06-07 19:11:42 +0200

    Added progress bar

--- app/src/main/java/com/example/filedownloader/MainActivity.java
+++ app/src/main/java/com/example/filedownloader/MainActivity.java
@@ -7,6 +7,7 @@ import android.annotation.SuppressLint;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
     Button mDownloadBtn;
     EditText mInputLink;
 
+    DownloadManager downloadmanager;
+
+    Boolean dataRecieved;
+
+
     final static String TEST_LINK = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.6.15.tar.xz";
 
     final static int PERM_CODE = 1000;
@@ -47,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
         mInputLink = findViewById(R.id.inputTextField);
 
         mInputLink.setText(TEST_LINK);
+
+        dataRecieved = false;
+
 
         setListeners();
     }
@@ -83,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
                         ///perm denied
                     } else {
 
-                        Toast.makeText(getBaseContext(), "Zaczynam pobieranie", Toast.LENGTH_SHORT).show();
                         download(mLink);
                     }
                 }
@@ -110,18 +119,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void download(String link){
 
+            Toast.makeText(getBaseContext(), "Zaczynam pobieranie", Toast.LENGTH_SHORT).show();
 
-        DownloadManager downloadmanager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-        Uri uri = Uri.parse(link);
+            downloadmanager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+            Uri uri = Uri.parse(link);
 
-        DownloadManager.Request request = new DownloadManager.Request(uri);
-        request.setTitle("My File");
-        request.setDescription("Downloading");
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setVisibleInDownloadsUi(false);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, ""+System.currentTimeMillis() );
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+            request.setTitle("My File");
+            request.setDescription("Downloading");
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setVisibleInDownloadsUi(false);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, ""+System.currentTimeMillis() );
 
-        downloadmanager.enqueue(request);
+            downloadmanager.enqueue(request);
+
+            progress();
+
     }
 
     Boolean validate(String link){
@@ -159,13 +172,15 @@ public class MainActivity extends AppCompatActivity {
             try {
                 URL url = new URL(str);
                 polaczenie = (HttpURLConnection) url.openConnection();
-                polaczenie.setRequestMethod("GET");
-                polaczenie.setDoOutput(true);
-                String mRozmiar = polaczenie.getResponseMessage();
+                //polaczenie.setRequestMethod("GET");
+                //polaczenie.setDoOutput(true);
+                int mRozmiar = polaczenie.getContentLength();
                 String mTyp = polaczenie.getContentType();
 
-                mRozmiarTextView.setText( mRozmiar );
+                mRozmiarTextView.setText( String.valueOf(mRozmiar) );
                 mTypTextView.setText(mTyp);
+
+                dataRecieved = true;
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -176,4 +191,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    void progress(){
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                boolean downloading = true;
+
+                final ProgressBar pb = findViewById(R.id.progressBar2);
+
+                while (downloading) {
+
+                    DownloadManager.Query q = new DownloadManager.Query();
+                    Cursor cursor = downloadmanager.query(q);
+                    cursor.moveToFirst();
+
+                    int bytes_downloaded = cursor.getInt(cursor
+                            .getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                    int bytes_total = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES)); //Na emulatorze zwraca 0 z niewiadomego dla mnie powodu na moim urządzeniu działa prawidłowo
+
+                    if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                        downloading = false;
+                    }
+
+                    final int dl_progress = (int) ((bytes_downloaded * 100l) / bytes_total);
+
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            pb.setProgress((int) dl_progress);
+
+                        }
+                    });
+
+                    cursor.close();
+                }
+
+            }
+        }).start();
+    }
+
 }
